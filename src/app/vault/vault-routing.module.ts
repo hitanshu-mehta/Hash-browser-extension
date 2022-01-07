07bbeccf990e6a054da474b659df66b4eb3d3cad
Hitanshu Mehta <hitanshu99amehta@gmail.com>
2022-01-07 19:32:54 +0530

    clean up vault module to start from beginning
    Signed-off-by: Hitanshu Mehta <hitanshu99amehta@gmail.com>

--- src/app/vault/vault-routing.module.ts
+++ src/app/vault/vault-routing.module.ts
@@ -1,15 +1,13 @@
-import { AddVaultItemComponent } from './containers/add-vault-item/add-vault-item.component';
+import { VaultItemViewComponent } from './components/vault-item-view/vault-item-view.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { SelectedVaultItemComponent } from './containers/selected-vault-item/selected-vault-item.component';
 import { VaultListComponent } from './components/vault-list/vault-list.component';
 
 const vaultRoutes: Routes = [
     { path: 'vault-list', redirectTo: 'main' },
     { path: 'vault-list', component: VaultListComponent },
-    { path: 'vault-item/:id', component: SelectedVaultItemComponent },
-    { path: 'new-vault-item', component: AddVaultItemComponent }
+    { path: 'vault-item/:id', component: VaultItemViewComponent },
 ];
 
 
